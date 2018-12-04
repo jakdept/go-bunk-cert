3f6eadbabe89536a228ed0967934775df2ad30bd
Jack Hayhurst <jakdept@gmail.com>
2018-12-04 11:51:10 -0500

    finished further changes
    Signed-off-by: Jack Hayhurst <jakdept@gmail.com>

--- selfCert.go
+++ selfCert.go
@@ -11,46 +11,52 @@ import (
 	"crypto/x509/pkix"
 	"encoding/pem"
 	"fmt"
-	"log"
 	"math/big"
 	"os"
 	"time"
 )
 
-type SSLConfig struct {
-	Host     string
-	RsaBits  int
-	ValidFor time.Duration
-}
-
-func GenerateRsa(host string, rsaBits int, validFor time.Duration) ([]byte, []byte, error) {
-	var err error
+// GenerateRsa generates an RSA x509 key and certificate, for use as a self signed SSL.
+func GenerateRsa(host string, rsaBits int, validFor time.Duration) (cert []byte, key []byte, err error) {
 
 	if host == "" {
 		host, err = os.Hostname()
 		if err != nil {
 			return []byte{}, []byte{}, fmt.Errorf(
-				"no hostname give, failed to retrieve current hostname - %v", err)
+				"no hostname given and failed to retrieve local: %v", err)
 		}
 	}
 
-	priv, err := rsa.GenerateKey(rand.Reader, rsaBits)
-	if err != nil {
-		log.Fatalf("failed to generate private key: %s", err)
+	if rsaBits == 0 {
+		rsaBits = 4096
+	}
+	if rsaBits != 1024 && rsaBits != 2048 && rsaBits != 3072 && rsaBits != 4096 {
+		return []byte{}, []byte{}, fmt.Errorf(
+			"invalid rsa key size - %d", rsaBits)
+	}
+
+	if validFor == time.Duration(0) {
+		validFor = time.Hour * 24 * 365
 	}
 	notBefore := time.Now()
 
 	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
 	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
 	if err != nil {
-		log.Fatalf("failed to generate serial number: %s", err)
+		return []byte{}, []byte{}, fmt.Errorf(
+			"failed to generate serial number: %v", err)
+	}
+
+	priv, err := rsa.GenerateKey(rand.Reader, rsaBits)
+	if err != nil {
+		return []byte{}, []byte{}, fmt.Errorf(
+			"failed to generate private key: %v", err)
 	}
 
 	template := x509.Certificate{
 		SerialNumber: serialNumber,
-		Subject: pkix.Name{
-			Organization: []string{"Acme Co"},
-		},
+		Subject:      pkix.Name{Organization: []string{"Acme Co"}},
+		DNSNames:     []string{host},
 
 		NotBefore: notBefore,
 		NotAfter:  notBefore.Add(validFor),
@@ -62,16 +68,19 @@ func GenerateRsa(host string, rsaBits int, validFor time.Duration) ([]byte, []by
 		ExtKeyUsage: []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
 	}
 
-	derBytes, err := x509.CreateCertificate(rand.Reader, &template, &template, priv.PublicKey, priv)
+	pubKeyBytes, err := x509.CreateCertificate(rand.Reader, &template, &template, priv.PublicKey, priv)
 	if err != nil {
-		log.Fatalf("Failed to create certificate: %s", err)
+		return []byte{}, []byte{}, fmt.Errorf(
+			"failed to create certificate: %v", err)
 	}
 
-	cert := pem.EncodeToMemory(&pem.Block{
+	cert = pem.EncodeToMemory(&pem.Block{
 		Type:  "CERTIFICATE",
-		Bytes: derBytes})
-	key := pem.EncodeToMemory(&pem.Block{
+		Bytes: pubKeyBytes,
+	})
+	key = pem.EncodeToMemory(&pem.Block{
 		Type:  "RSA PRIVATE KEY",
-		Bytes: x509.MarshalPKCS1PrivateKey(k)})
+		Bytes: x509.MarshalPKCS1PrivateKey(priv),
+	})
 	return cert, key, nil
 }
